caf59a4f40392c42aee2e025b549a7ea2e9a1a28
orekentaro <kntru0218gj@gmail.com>
2022-06-29 23:34:03 +0900

    消去

--- models/db.py
+++ models/db.py
@@ -4,22 +4,24 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 DATABASE = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
-    'user': 'nagatokentarou',
-    'password': 'nagato8',
-    'host': '127.0.0.1',
-    'name': 'management_recruit'
+    'user': '',
+    'password': '',
+    'host': '',
+    'name': ''
 })
 
 ENGINE = create_engine(
     DATABASE,
     echo=True
 )
-db_session = scoped_session(
+
+session = scoped_session(
     sessionmaker(
         autoflush=True,
         bind=ENGINE,
-        autocommit=False)
+        autocommit=False,
+        expire_on_commit=False
+        )
 )
 
 Base = declarative_base()
-Base.query = db_session.query_property()
